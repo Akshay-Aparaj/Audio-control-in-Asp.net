ca4f9be1e2217352c4b75cc6c114a07be09b5364
Akshay-Aparaj <64407055+Akshay-Aparaj@users.noreply.github.com>
2021-03-01 15:56:24 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsVolumeControl
+{
+    public partial class Form1 : Form
+    {
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+        WebCamera oWebCam;
+        private PictureBox pictureBox1;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            oWebCam = new WebCamera();
+            oWebCam.Container = pictureBox1;
+        }
+       
+
+        private void btnMute_Click(object sender, EventArgs e)
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_MUTE);
+        }
+
+        private void btnDecVol_Click(object sender, EventArgs e)
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_DOWN);
+        }
+
+        private void btnIncVol_Click(object sender, EventArgs e)
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_UP);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oWebCam = new WebCamera();
+            oWebCam.Container = pictureBox2;
+            oWebCam.OpenConnection();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            oWebCam.SaveImage();
+            pictureBox2.Image = null;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            oWebCam.Dispose();
+        }
+    }
+}
